ff63291d27906e26b7c26c94b7e32314097d1096
Calil-Silva <ka.renner@hotmail.com>
2021-12-09 23:46:45 -0300

    Fixed pre-hooks

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
-import './setup';
 import app from './app';
+import './setup';
 
-app.listen(process.env.PORT, () =>
-  console.log(`Listening on port ${process.env.PORT}`)
-);
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
