fec979741e5d8c97a6295076e034753f24d2e7fb
Bruna Topan <brutopanb@gmail.com>
2021-12-17 11:31:14 -0300

    Preparando review

--- src/errors/QuestionCreationError.ts
+++
@@ -1,8 +0,0 @@
-class QuestionCreationError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'QuestionCreationError';
-  }
-}
-
-export default QuestionCreationError;
--- src/routers/usersRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as usersController from '../controllers/usersController';
-
-const router = express.Router();
-
-router.post('/', usersController.createNewUser);
-
-export default router;
